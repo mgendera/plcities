dc5c5b1d9ae87622a8f6f8d8131a896cc40c80ff
mgendera <62478705+mgendera@users.noreply.github.com>
2020-04-05 02:30:18 +0200

    Add files via upload

---
+++ plcities/R/woj.R
@@ -0,0 +1,28 @@
+#' Województwa
+#'
+#' @description Lista wszystkich województw wraz z powierzchnią w km2
+#'
+#' @param wojewodztwa lista
+#'
+#' @return Nazwa województwa wraz z powierzchnią w postaci listy#'
+#' @export
+#'
+#' @examples
+#' wojewodztwa
+
+wojewodztwa = list(c('ŚLĄSKIE', 12333),
+         c('WIELKOPOLSKIE', 29826),
+         c('ZACHODNIOPOMORSKIE', 22892),
+         c('PODLASKIE', 20187),
+         c('POMORSKIE', 18310),
+         c('KUJAWSKO-POMORSKIE', 17792),
+         c('DOLNOŚLĄSKIE', 19947),
+         c('ŁÓDZKIE', 18219),
+         c('MAŁOPOLSKIE', 15183),
+         c('MAZOWIECKIE', 35558),
+         c('PODKARPACKIE', 17846),
+         c('LUBELSKIE', 25122),
+         c('OPOLSKIE', 9412),
+         c('ŚWIĘTOKRZYSKIE', 11711),
+         c('LUBUSKIE', 13988),
+         c('WARMIŃSKO-MAZURSKIE', 24173))
